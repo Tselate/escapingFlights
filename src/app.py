fd2eb0483fd3b1cd4784935cadb65fd22cacaff6
Tselate <tselatedawit@outlook.com>
2024-08-09 16:35:12 +0900

    Updated app

--- src/app.py
+++ src/app.py
@@ -4,16 +4,24 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     return '''
      <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
+        <label for="start_date">Start Date:</label>
+        <input type="date" id="start_date" name="start_date" required>
+        
+        <label for="city">City:</label>
+        <input type="text" id="city" name="city" required>
+        
+        <input type="submit" value="Submit!">
      </form>
      '''
 
+
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    input_date = request.form.get("start_date", "")
+    input_city = request.form.get("city", "")
+    return "Your flight date is " + input_date + " and the city you are flying out of is " + input_city
